a932627e3204a0e243e2660579f0f32e143eac37
anudeep646 <anudeep646@gmail.com>
2017-01-11 16:13:18 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-<job name="defaultJob">
-  <tasks> 
-    <exec command-"/bin/bash">
-      <arg>-c</arg>
-      <arg>curl -o downloadedFile ${JENKINS_URL}job/${JENKINS_JOB_NAME}/${JENKINS_JOB_NEMBER}/artifact/${JENKINS_ARTIFACT}</arg>
-    </exec>
-   </tasks>
- </job>
+#<job name="defaultJob">
+ # <tasks> 
+  #  <exec command-"/bin/bash">
+   #   <arg>-c</arg>
+    #  <arg>curl -o downloadedFile ${JENKINS_URL}job/${JENKINS_JOB_NAME}/${JENKINS_JOB_NEMBER}/artifact/${JENKINS_ARTIFACT}</arg>
+    #</exec>
+   #</tasks>
+ #</job>
+curl -o downloadedFile ${JENKINS_URL}job/${JENKINS_JOB_NAME}/${JENKINS_JOB_NEMBER}/artifact/${JENKINS_ARTIFACT}
